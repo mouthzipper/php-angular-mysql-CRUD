227b51eb62b6f9a94ae085c422a65bd063f91ced
mouthzipper <ramos.jeromeh@gmail.com>
2015-04-25 22:49:14 +0800

    add ui bootstrap

--- assets/js/config/config.module.js
+++ assets/js/config/config.module.js
@@ -1,6 +1,6 @@
 (function() {
 	'use strict';
 
-	angular.module( 'app.config', [ 'ui.router', 'angular-loading-bar' ] )
+	angular.module( 'app.config', [ 'ui.router', 'angular-loading-bar', 'ui.bootstrap' ] )
 	.constant('BASE_URL', 'http://localhost/php-crud/');
 })();
